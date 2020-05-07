6a5a4d2ad39c18942cbf6305c65e77f482ae099f
azolnoni <[azizeoloubokiki93@gmail.com]>
2020-05-07 05:53:09 +0200

    add a function who find sonond hight number inside array

--- src/com/algosolution/AlgoSolution.java
+++ src/com/algosolution/AlgoSolution.java
@@ -15,29 +15,50 @@ public class  AlgoSolution {
 
          return resultat;
      }
-
-
-
-    public static int MaximumNumber(int[] numbers){
-
-        int max=0;
-
-        for (int j=0;j<numbers.length;j++){
-            if(max<numbers[j]){
-                max=numbers[j];
+    public static int MaximumNumber(int[] numbers) {
+        int max = 0;
+        for (int j = 0; j < numbers.length; j++) {
+            if (max<numbers[j]) {
+                    max = numbers[j];
             }
         }
-        return max;
+            return max;
+        }
+
+
+
+    public static int SecondMaximumNumber(int[] numbers) {
+        int max1 = 0;
+        int max2 = 0;
+        for (int j = 0; j < numbers.length; j++) {
+            if (max1 < numbers[j] && max2 < numbers[j]) {
+                if (max2 > max1) {
+                    max1 = max2;
+                    max2 = numbers[j];
+                } else {
+                    max2 = max1;
+                    max1 = numbers[j];
+                }
+            } else {
+                if (max1 > numbers[j] && max2 < numbers[j]) {
+                    max2 = numbers[j];
+                } else {
+                    if (max1 < numbers[j] && max2 > numbers[j]) {
+                        max1 = max2;
+                    }
+                }
+            }
+        }
+        return max2;
     }
 
 
-
-
     public static void main(String[] args) {
-        int[] numbers=new int[]{94000 ,150,70000,9,2000,500,1500,4,30000};
+        int[] numbers=new int[]{9000000 ,150,70000000,8000000,2000,500,1500,4,30000000};
 
         System.out.println(FIND(0, 4, "hello word!"));
-        System.out.print(MaximumNumber(numbers));
+        System.out.println(MaximumNumber(numbers));
+        System.out.print(SecondMaximumNumber(numbers));
 
     }
 
